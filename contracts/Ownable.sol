0aff92c521dc4047d193d3c7c74de40aae2a88c6
ice-charon <antoniy.shumanov@leftclick.io>
2025-12-04 08:53:00 +0200

    use multisig address as owner

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -3,29 +3,15 @@ pragma solidity ^0.7.0;
 
 // This contract handles the owner logic
 abstract contract Ownable {
-    // Stored as 'private immutable' for gas efficiency
-    address private immutable _owner;
+    // mutisig address
+    address public constant owner = 0xDFDe8108E14c70B6796bdd220454A80E849C7689;
 
     string public constant ERROR_NOT_OWNER = "Caller is not the owner";
 
-    // Event to log the ownership transfer
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    // Set the contract deployer as the initial owner
-    constructor() {
-        _owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
-    }
-
     // The core access control modifier
     modifier onlyOwner() {
-        require(msg.sender == _owner, ERROR_NOT_OWNER);
+        require(msg.sender == owner, ERROR_NOT_OWNER);
         // The '_' executes the body of the function it is attached to
         _;
-    }
-
-    // Function to allow getting the owner's address
-    function owner() public view virtual returns (address) {
-        return _owner;
     }
 }
